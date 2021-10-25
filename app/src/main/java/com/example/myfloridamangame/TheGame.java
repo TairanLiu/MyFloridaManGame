520afed88f62d9878042c6864fafcc8d39454fd0
liut <cutemolang@126.com>
2021-10-25 13:12:08 -0400

    how to load screen

--- app/src/main/java/com/example/myfloridamangame/TheGame.java
+++ app/src/main/java/com/example/myfloridamangame/TheGame.java
@@ -22,14 +22,16 @@ public class TheGame extends AppCompatActivity {
     Button D;
     static int score = 0;
     String selectedAnswer;
-    Iterator i = HeadlineBank.bank().values().iterator();
+    Iterator<Map.Entry<Integer, Headline>>  i;
     Headline currentHeadlne;
     boolean outOfQuestions = false; //So we know when to stop playing
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_game);
+        i = HeadlineBank.hdlBank.entrySet().iterator();
         //getNextEntry(); // Mr. U - We set currentHeadline to the first headline found in the HashMap. Without this being called, we get a NullPointerException
         question = findViewById(R.id.textView);
         A = findViewById(R.id.button);
@@ -47,8 +49,10 @@ public class TheGame extends AppCompatActivity {
 
 
     public void getNextEntry() {
-        while (i.hasNext()) {
-            currentHeadlne = (Headline)  i.next(); //Mr U - Cast Not Needed afterall
+        Integer x = 0;
+        if (i.hasNext()) {
+            Map.Entry<Integer, Headline> entry = (Map.Entry<Integer, Headline>) i.next();
+            currentHeadlne = entry.getValue();
         }
     }
 
@@ -56,10 +60,10 @@ public class TheGame extends AppCompatActivity {
         if(i.hasNext()) {
             return false;
         }
-        else {
+        else
             return true;
         }
-    }
+
 
     public void askQuestion( Headline h ){
         question.setText(h.getStory());
@@ -70,47 +74,62 @@ public class TheGame extends AppCompatActivity {
     }
 
     public void onClickA(View view){
-        iterate();
         System.out.print("life in general");
         Log.d("Button","clicked");
         if (currentHeadlne.getBlank().equals(A.getText())){
             score++;
-
         }
+        getNextEntry();
+        askQuestion(currentHeadlne); //Mr. U - You play a "round"
+        iterate();
     }
     public void onClickB(View view){
-        iterate();
+
         System.out.print("life in general");
         if (currentHeadlne.getBlank().equals(B.getText())){
             score++;
 
         }
+        getNextEntry();
+        askQuestion(currentHeadlne); //Mr. U - You play a "round"
+        iterate();
     }
     public void onClickC(View view){
-        iterate();
+
         System.out.print("life in general");
         if (currentHeadlne.getBlank().equals(C.getText())){
             score++;
 
         }
+        getNextEntry();
+        askQuestion(currentHeadlne); //Mr. U - You play a "round"
+        iterate();
     }
     public void onClickD(View view){
-        iterate();
+
         System.out.print("life in general");
         if (currentHeadlne.getBlank().equals(D.getText())){
             score++;
 
         }
+        getNextEntry();
+        askQuestion(currentHeadlne); //Mr. U - You play a "round"
+        iterate();
     }
     public void loadScreen(){
+        Log.d("screen","go on");
         Intent intent = new Intent (this, Final.class);
+        //take from this current view to theGame
         startActivity(intent);
     }
     public static int score(){
         return score;
     }
+
     public void iterate(){
         if (i.hasNext()) {
+            Log.d("Iterator","Should load next question");
+
             getNextEntry(); // Mr. U - We set currentHeadline to the first headline found in the HashMap. Without this being called, we get a NullPointerException
             askQuestion(currentHeadlne); //Mr. U - You play a "round"
             Log.d("life","gone");
@@ -118,7 +137,13 @@ public class TheGame extends AppCompatActivity {
         }
         else{
             Log.d("Life","remain");
+            loadScreen();
         }
     }
+    /*public void loadScreen(){
+        Intent intent = new Intent (this, TheGame.class);
+        //take from this current view to theGame
+        startActivity(intent);
+    }*/
 
 }
