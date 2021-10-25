952de81d82151c4470ee1e1c3b077e22209a37e8
liut <cutemolang@126.com>
2021-10-25 15:08:51 -0400

    how to load screen

--- app/src/main/java/com/example/myfloridamangame/Final.java
+++ app/src/main/java/com/example/myfloridamangame/Final.java
@@ -14,7 +14,7 @@ public class Final extends AppCompatActivity {
         setContentView(R.layout.activity_final);
         finalScore = findViewById(R.id.textView4);
         bestScore = findViewById(R.id.textView6);
-        getFinalScore();
+        finalScore.setText(TheGame.score());
         bestScore.setText(getBestScore());
 
     }
